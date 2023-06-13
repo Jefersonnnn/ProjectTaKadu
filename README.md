ee01c092c5823587f8e6d834c5ae3d6bc1f16c0a
jeferson.machado <jeferson_m94@hotmail.com>
2023-06-13 09:11:27 -0300

    v1.0 funcional

--- README.md
+++ README.md
@@ -25,13 +25,47 @@ port = your_port
 PATH_FILE_ID_SENSORS = ./data/sensors.csv
 PATH_FOLDER_OUT = ./out
 ```
+
+<h3>Configurações via variáveis de ambiente (DB)</h3>
+
+Abra o Prompt de Comando e execute o seguinte comando:
+Windows cmd:
+```shell
+setx PG_DATABASE "database_name"
+setx PG_HOST "hostname"
+setx PG_PORT "port"
+setx PG_USER "your_user_db"
+setx PG_PASSWORD "your_password_db"
+```
+Windows powershell
+```shell
+$env:PG_DATABASE="database_name"
+$env:PG_HOST="hostname"
+$env:PG_PORT="port"
+$env:PG_USER="your_user_db"
+$env:PG_PASSWORD="your_password_db"
+```
+Linux e MacOS:
+```shell
+export PG_DATABASE="database_name"
+export PG_HOST="hostname"
+export PG_PORT="port"
+export PG_USER="your_user_db"
+export PG_PASSWORD="your_password_db"
+```
+
 <p> Para as notificações de erro funcionarem será necessário definir variáveis de ambiente.</p>
 
 Abra o Prompt de Comando e execute o seguinte comando:
-Windows:
+Windows cmd:
 ```shell
 setx EMAIL_USERNAME "your_email_username"
 setx EMAIL_PASSWORD "your_email_password"
+```
+Windows powershell
+```shell
+$env:EMAIL_USERNAME "your_email_username"
+$env:EMAIL_PASSWORD "your_email_password"
 ```
 Linux e MacOS:
 ```shell
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import datetime
+import ftplib
 import os
+
 import zipfile
 import csv
 import configparser
 from pathlib import Path
+from typing import Any
 
 import psycopg2
 import pandas as pd
@@ -11,12 +14,15 @@ import pandas as pd
 from redmail import EmailSender
 
 from rocketry import Rocketry
-from rocketry.conds import retry,  every
+from rocketry.conds import retry, every
+
+from client_ftp import ClientFTP
 
 app = Rocketry(execution='async')
 
 ############# LOGGING
 import logging
+
 logger = logging.getLogger("rocketry.scheduler")
 logger.setLevel(logging.INFO)
 
@@ -35,9 +41,10 @@ global PATH_FILE_ID_SENSORS
 
 from logging import Handler
 
+
 class EmaillNotifyHandler(Handler):
 
-    def __init__(self, level:int=logging.NOTSET, email=None):
+    def __init__(self, level: int = logging.NOTSET, email=None):
         Handler.__init__(self, level)
 
         if not email:
@@ -45,11 +52,11 @@ class EmaillNotifyHandler(Handler):
             raise Exception("attr 'email' não informado!")
         self._email = email
 
-    def emit(self,record:logging.LogRecord):
+    def emit(self, record: logging.LogRecord):
         self._email.send(
             subject=f'Takadu Service - Log Record: {record.levelname}',
-            text=f"Logging level: { record.levelname }\nMessage: { record.message }",
-            html=f"<ul><li>Logging level: { record.levelname }</li><li>Message: { record.message }</li></ul>",
+            text=f"Logging level: {record.levelname}\nMessage: {record.message}",
+            html=f"<ul><li>Logging level: {record.levelname}</li><li>Message: {record.message}</li></ul>",
             attachments={
                 'app.log': Path("app.log")
             }
@@ -61,26 +68,33 @@ def initial_config(config_file_path):
         config = configparser.ConfigParser()
         config.read(config_file_path)
 
+        # Postgres Config
         os.environ['PG_DATABASE'] = os.environ.get('PG_DATABASE', config['postgresql']['database'])
         os.environ['PG_USER'] = os.environ.get('PG_USER', config['postgresql']['user'])
         os.environ['PG_PASSWORD'] = os.environ.get('PG_PASSWORD', config['postgresql']['password'])
         os.environ['PG_HOST'] = os.environ.get('PG_HOST', config['postgresql']['host'])
         os.environ['PG_PORT'] = os.environ.get('PG_PORT', config['postgresql']['port'])
 
+        # FTP Config
+        os.environ['FTP_HOST'] = os.environ.get('FTP_HOST', config['ftp']['host'])
+        os.environ['FTP_USER'] = os.environ.get('FTP_USER', config['ftp']['username'])
+        os.environ['FTP_PASSWORD'] = os.environ.get('FTP_PASSWORD', config['ftp']['password'])
+        os.environ['FTP_DIR_AGUA'] = os.environ.get('FTP_DIR_AGUA', config['ftp']['agua_dir'])
+        os.environ['FTP_DIR_ESGOTO'] = os.environ.get('FTP_DIR_ESGOTO', config['ftp']['esgoto_dir'])
+
         PATH_FOLDER_OUT = config['default']['PATH_FOLDER_OUT']
         PATH_FILE_ID_SENSORS = config['default']['PATH_FILE_ID_SENSORS']
 
-        error_notify_email_username =  os.environ.get('EMAIL_USERNAME', config['notify_outlook']['username'])
-        error_notify_email_password =  os.environ.get('EMAIL_PASSWORD', config['notify_outlook']['password'])
-
+        error_notify_email_username = os.environ.get('EMAIL_USERNAME', config['notify_outlook']['username'])
+        error_notify_email_password = os.environ.get('EMAIL_PASSWORD', config['notify_outlook']['password'])
 
         email = EmailSender(
-                host="smtp.office365.com",
-                port=587,
-                # use_starttls=True,
-                username=error_notify_email_username,
-                password=error_notify_email_password,
-            )
+            host="smtp.office365.com",
+            port=587,
+            # use_starttls=True,
+            username=error_notify_email_username,
+            password=error_notify_email_password,
+        )
 
         email.receivers = config['notify_outlook']['receivers']
 
@@ -116,12 +130,13 @@ def connect_to_postgres():
         return None
 
 
-def load_csv_list_sensors(path_file:str, delimiter:str=";") -> list:
+def load_csv_list_sensors(path_file: str, delimiter: str = ";") -> tuple[Any, Any]:
     df_sensors = pd.read_csv(path_file, delimiter=delimiter)
     if not "Sensor" in df_sensors.columns:
         logger.error(f"'Sensor' column not found in file: {path_file}")
         raise Exception(f"'Sensor' column not found in file: {path_file}")
-    return df_sensors["Sensor"].tolist()
+    return df_sensors.loc[df_sensors["type"] == 'AGUA', "Sensor"].tolist(), \
+        df_sensors.loc[df_sensors["type"] == 'ESGOTO', "Sensor"].tolist()
 
 
 def _fetchall_to_csv(rows, header, csv_file):
@@ -131,26 +146,27 @@ def _fetchall_to_csv(rows, header, csv_file):
         writer.writerows(rows)
 
 
-def load_data_from_db(conn, table_name, ids_sensors:list) -> tuple:
-
+def load_data_from_db(conn, table_name, ids_sensors: list) -> tuple:
     if conn and table_name and ids_sensors and isinstance(ids_sensors, list):
         logger.info("Searching for new data (24hours) in the database")
-        ids_sensors = ','.join(str(x) for x in ids_sensors)
+        list_ids_sensors = ','.join(str(x).replace("SEN_", "") for x in ids_sensors)
 
         try:
             with conn.cursor() as cursor:
                 query = f"""
                         SELECT
-                            m.pointconfig_id Id
+                            'SEN_' || pc.id Name
                             ,m.measure_datetime Timestamp
                             ,m.calculated_measure Value
                         
                         FROM
                         public.measure m
                         
+                        INNER JOIN pointconfig pc ON pc.id = m.pointconfig_id
+                        
                         WHERE
                             m.measure_datetime between NOW() - interval '24 hours' and NOW()
-                            AND m.pointconfig_id IN ({ids_sensors})
+                            AND m.pointconfig_id IN ({list_ids_sensors})
                             
                             ORDER BY measure_datetime DESC
                         """
@@ -165,14 +181,17 @@ def load_data_from_db(conn, table_name, ids_sensors:list) -> tuple:
             return False, False
 
 
-def save_list_to_csv_and_zip(data_list:list, header:list, data_source_name="TELELOG", destination_folder="./out", zip_file=True):
+def save_list_to_csv_and_zip(data_list: list, header: list, _type: str, data_source_name="TELELOG",
+                             destination_folder=".\\out",
+                             zip_file=True):
     """
     Save a list to .csv file and compress it in a .zip file.
     The .csv file will have a dynamic name with the following format: "DataSourceName_YYYYMMDDHH24MMSS.csv".
-    :param data_list: lista retornada da função cursor.fetchall() "Lista com tuplas"
+    :param _type: System Type - Water or Sewage (AGUA, ESGOTO)
+    :param data_list: list returned from cursor.fetchall() function "List with tuples"
     :param data_source_name: Name of the data source that will be used as a prefix in the .csv file name
     :param destination_folder: Folder where the .zip file will be saved
-    :param zip_file:
+    :param zip_file: If it is necessary to "zip" the file
     """
     if not all(isinstance(i, tuple) for i in data_list):
         raise Exception("data_list not is a instance of 'list'")
@@ -190,27 +209,68 @@ def save_list_to_csv_and_zip(data_list:list, header:list, data_source_name="TELE
             logger.error("save_list_to_csv_and_zip: File .csv not found.")
             raise Exception("File .csv not found.")
 
-        zip_file_name = f"{csv_file}.zip"
+        zip_file_name = f"{_type}-{csv_file}.zip"
         path_to_save_zip = os.path.join(destination_folder, zip_file_name)
         with zipfile.ZipFile(path_to_save_zip, 'w', zipfile.ZIP_DEFLATED) as myzip:
             myzip.write(path_to_save_csv, arcname=csv_file + ".csv")
 
         os.remove(path_to_save_csv)
 
-@app.task(every('15 minute') | retry(3))
+    # Client FTP
+    ftp_client = ClientFTP(
+        user=os.environ.get('FTP_USER'),
+        password=os.environ.get('FTP_PASSWORD'),
+        host=os.environ.get('FTP_HOST'),
+        port=22,
+        tls_ssl=True
+    )
+
+    ftp_client.connect()
+    if _type == 'ESGOTO':
+        ftp_client.upload(zip_file_name, path_to_save_zip, dest_path='./upload/esgoto')
+    else:
+        ftp_client.upload(zip_file_name, path_to_save_zip, dest_path='./upload')
+    ftp_client.quit()
+
+    delete_files_in_folder(destination_folder)
+
+
+def delete_files_in_folder(folder_path: str):
+    """
+    Deleta todos os arquivos de uma pasta
+    :param folder_path: Caminho da pasta.
+    """
+    try:
+        file_list = os.listdir(folder_path)
+        for file_name in file_list:
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        print(f"Todos os arquivos em {folder_path} foram deletados.")
+    except OSError as e:
+        print(f"Erro ao deletar os arquivos em {folder_path}: {e}")
+
+
+@app.task(every('10 minute') | retry(3))
 async def run_app():
     logger.info("Iniciando carregamento dos dados...")
     start_time = datetime.datetime.now()
 
-    list_ids_sensors = load_csv_list_sensors(PATH_FILE_ID_SENSORS)
+    list_ids_agua, list_ids_esgoto = load_csv_list_sensors(PATH_FILE_ID_SENSORS)
     conn = connect_to_postgres()
-    data, header = load_data_from_db(conn, "measure", ids_sensors=list_ids_sensors)
-    save_list_to_csv_and_zip(data_list=data, header=header)
+    data_agua, header_agua = load_data_from_db(conn, "measure", ids_sensors=list_ids_agua)
+    data_esgoto, header_esgoto = load_data_from_db(conn, "measure", ids_sensors=list_ids_agua)
+
+    if data_agua:
+        save_list_to_csv_and_zip(data_list=data_agua, header=header_agua, _type='AGUA')
+    if data_esgoto:
+        save_list_to_csv_and_zip(data_list=data_esgoto, header=header_esgoto, _type='ESGOTO')
 
     end_time = datetime.datetime.now()
 
-    exec_time = round((end_time - start_time).total_seconds(),2)
+    exec_time = round((end_time - start_time).total_seconds(), 2)
     logger.info(f"Tempo para carregar os dados: {exec_time} segundos")
+
 
 def main():
     print("Serviço TaKaDu Load Data iniciado...")
